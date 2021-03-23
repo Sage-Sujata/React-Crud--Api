24d1f08a3e5a2a79b71d7118436ce71eb7011d80
Sage-Sujata <sage.s@appwrk.com>
2021-03-24 00:03:31 +0530

    this is react crud api

---
+++ src/components/users/User.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+const User = () => {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    webiste: "",
+  });
+  const { id } = useParams();
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+
+
+  const { name } = useParams();
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+
+
+
+
+  const loadUser = async () => {
+    const res = await axios.get(`http://localhost:5001/test/${id}`);
+    
+
+    console.log("Rest or data");
+    setUser(res.data);
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <div className="container py-4">
+      <Link className="btn btn-primary" to="/">
+        back to Home
+      </Link>
+      <h1 className="display-4">User Detail </h1>
+      <hr />
+      <ul className="list-group w-50">
+        <li className="list-group-item">Name: {user.name}</li>
+        <li className="list-group-item">User Name: {user.username}</li>
+        <li className="list-group-item">Email: {user.email}</li>
+        <li className="list-group-item">Phone: {user.phone}</li>
+        <li className="list-group-item">Website: {user.website}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default User;
